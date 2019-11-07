eaa19406502e69e8dc9961b13b79d9ab26f1c76e
chrstphgrnr <christophgruener@me.com>
2019-11-07 15:45:12 -0700

    add button funtion

--- src/index.js
+++ src/index.js
@@ -7,8 +7,22 @@ import ReactDOM from 'react-dom';
 
 // Create React Component
 
+const getButtonText = () => {
+    return "Submit THIS!"
+}
+
 const App = () => {
-    return <div>Hi there!</div>
+    
+    
+    return (
+        <div>
+        <label className="label" for="name"> Enter Name </label>
+        <input id="name" type="text"/>
+        <button style={{backgroundColor: 'blue', color: 'white'}}>
+            {getButtonText()}
+        </button>
+        </div>
+    );
 }
 
 // Take the React Component and show it on the Screen
